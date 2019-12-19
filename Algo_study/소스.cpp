2578bad5e4f542fd7522a5b8fc4f240a20c06147
qazqwerqaz <43169966+qazqwerqaz@users.noreply.github.com>
2019-12-19 18:16:56 +0900

    19.12.18.
    가장 긴 바이토닉 부분수열
    LIS문제 응용

--- Algo_study/소스.cpp
+++ Algo_study/소스.cpp
@@ -1,6 +1,57 @@
-#include<iostream>
+#include <iostream>
 
-int main()
+
+int find_LIS(int);
+int find_LDS(int);
+
+using namespace std;
+
+int _size=0;
+int maxS;
+int A[1000]= {-1,};
+
+int main(int argc, char* argv[]) {
+	
+
+	cin>>_size;
+	
+	for(int i=0;i<_size;++i)
+	{
+		cin>>A[i];
+	}
+	
+	cout<<find_LIS(0)<<endl;
+	cout<<find_LDS(_size)<<endl;
+	return 0;
+}
+
+int S_cache[1000];
+
+
+int find_LIS(int index)
 {
-	std::cout << "d";
+	int& ret = S_cache[index];
+	if(ret != 0) return ret;
+	ret=1;
+	for(int next=index+1; next<_size ;++next)
+		if(A[index]<A[next])
+		{
+			ret = max(ret,find_LIS(next)+1);
+		}
+	return ret;
+			
+}
+
+int D_cache[1000];
+int find_LDS(int index)
+{
+	int& _ret = D_cache[index];
+	if(_ret != 0) return _ret;
+	_ret=1;
+	for(int next=index-1;next>=0;--next)
+		if(A[index]>A[next])
+		{
+			_ret = max(_ret,find_LDS(next)+1);
+		}
+	return _ret;
 }
