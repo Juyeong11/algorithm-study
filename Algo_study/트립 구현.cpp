7d5fb65e41d27da90c83fd5b441a91099f930546
qazqwerqaz <43169966+qazqwerqaz@users.noreply.github.com>
2020-07-16 11:27:28 +0900

    trip tree

--- Algo_study/트립 구현.cpp
+++ Algo_study/트립 구현.cpp
@@ -21,24 +21,32 @@ struct Node {
 	{
 		size = 1;
 		if (left) size += left->size;
-		if (right)size += right->size; 
+		if (right)size += right->size;
 	}
 };
-
+/*
+노드를 추가 하기 위해서는 가장 먼저 root와 node의 우선순위를 확인해야 한다.
+만약 node의 우선순위가 더 높다면 node가 root가 되어야 하기 때문이다.
+이렇게 될 경우 기존에 이 트리에 있던 노드들은 모두 node의 자식이 되어야 한다.
+여기서 node의 값이 root의 값보다 크다면, root의 오른쪽 서브트리의 값들은 node의 값보다 클 수도 있고 작을 수도 있다.
+그러므로 root의 오른쪽 서브 트리의 값을 node보다 큰 값과 작은 값을 가진 각각의 서브트리로 쪼갠다음 작은 값을
+root의 오른쪽 서브트리로 붙이고 큰값을 node의 오른쪽 서브트리로 붙이면 삽입이 완료 된다.
+*/
 typedef pair<Node*, Node*> NodePair;
 
 NodePair split(Node* root, KeyType key)
 {
 	if (root == NULL) return NodePair(NULL, NULL);
 
+	//루트가 키 미만이면 오른쪽 서브트리를 쪼갠다.
 	if (root->key < key)
 	{
 		NodePair rs = split(root->right, key);
 		root->setRight(rs.first);
 		return NodePair(root, rs.second);
 	}
-
-	NodePair ls = split(root->left, key);
+	//루트가 키 이상이면 왼쪽 서브트리를 쪼갠다.
+	NodePair ls = (root->left, key);
 	root->setLeft(ls.second);
 	return NodePair(ls.first, root);
 }
@@ -57,12 +65,13 @@ Node* insert(Node* root, Node* node)
 		root->setLeft(insert(root->left, node));
 	else
 		root->setRight(insert(root->right, node));
-	return root;
+	return root;//root가 바뀔 수 있으니 반환값을 root로 해주어 바뀌는 root값을 알려준다.
 }
 
+//a와b가 두 개의 트립이고, max(a) < min(b) 일 때 이 둘을 합친다.
 Node* merge(Node* a, Node* b)
 {
-	if(a == NULL) return b;
+	if (a == NULL) return b;
 	if (b == NULL) return a;
 	if (a->priority < b->priority) {
 		b->setLeft(merge(a, b->left));
@@ -76,6 +85,7 @@ Node* erase(Node* root, KeyType key)
 {
 	if (root == NULL) return root;
 
+	//루트를 지우고 양 서브트리를 합친 뒤 반환한다.
 	if (root->key == key) {
 		Node* ret = merge(root->left, root->right);
 		delete root;
@@ -88,6 +98,7 @@ Node* erase(Node* root, KeyType key)
 	return root;
 }
 
+//k번째 원소를 반환한다.
 Node* kth(Node* root, int k)
 {
 	int leftSize = 0;
@@ -97,6 +108,7 @@ Node* kth(Node* root, int k)
 	return kth(root->right, k - leftSize - 1);
 }
 
+//key 보다 작은 키값의 수를 반환한다.
 int countLessThan(Node* root, KeyType key)
 {
 	if (root == NULL) return 0;
