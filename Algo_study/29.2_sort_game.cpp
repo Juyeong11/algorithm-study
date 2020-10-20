e32b9227c569b7a5e2c634512bb0a19313a7b934
qazqwerqaz <43169966+qazqwerqaz@users.noreply.github.com>
2020-10-20 11:27:31 +0900

    29.2 sort game
    모슨 순열에 대해 필요한 뒤집기 연산의 수를 계산해두는 방법

--- Algo_study/29.2_sort_game.cpp
+++ Algo_study/29.2_sort_game.cpp
@@ -44,3 +44,52 @@ int sort_game_bfs(const vector<int>& perm)
 	}
 	return -1;
 }
+
+
+map<vector<int>, int> toSort;
+
+void precalc(int n)
+{
+	vector<int> perm(n);
+
+	for (int i = 0; i < n; ++i) perm[i] = i;
+
+	queue<vector<int>> q;
+	q.push(perm);
+	toSort[perm] = 0;
+
+	while (!q.empty())
+	{
+		vector<int> here = q.front();
+		q.pop();
+		int cost = toSort[here];
+		for (int i = 0; i < n; ++i)
+			for (int j = i + 2; j <= n; ++j)
+			{
+				reverse(here.begin() + i, here.begin() + j);
+				if (toSort.count(here) == 0)
+				{
+					toSort[here] = cost + 1;
+					q.push(here);
+				}
+				reverse(here.begin() + i, here.begin() + j);
+			}
+	}
+}
+
+int solve(const vector<int>& perm)
+{
+	int n = perm.size();
+	vector<int> fixed(n);
+	for (int i = 0; i < n; ++i)
+	{
+		int smaller = 0;
+		for (int j = 0; j < n; ++j)
+		{
+			if (perm[j] < perm[i])
+				++smaller;
+		}
+		fixed[i] = smaller;
+	}
+	return toSort[fixed];
+}
